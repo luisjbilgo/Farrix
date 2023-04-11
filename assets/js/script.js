9871b276ff8a9f036b5ff9c8753f041334237d70
Luijo <luisjbilgo2004@gmail.com>
2023-04-11 01:04:22 -0500

    farrix sin animaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,59 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+/*equipo*/
+
+ 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+
+// Selecciona el elemento span
+const span = document.getElementById('oli');
+
+// Define un arreglo de textos que quieres mostrar
+const textos = ['Experiencias', 'Lugares', 'Amigos'];
+const nuevos = ['Nuevos ', 'Nuevas ', 'Nuevos '];
+
+let index = 0;
+
+// Función que actualiza el texto del span
+
+function cambiarTexto() {
+  span.textContent = textos[index];
+  index++;
+  
+  if (index >= textos.length) {
+    index = 0;
+  }
+}
+
+ // Cambia el texto cada 2 segundos
+setInterval(cambiarTexto, 2000);
